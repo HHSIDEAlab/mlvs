31b3971c9cdd8d491cb329ce18dc257e24133112
Alan Viars <alan.c.viars@gmail.com>
2013-12-30 18:09:36 -0500

    Added a simple ASCII front page to the service.

--- apps/ws/views.py
+++ apps/ws/views.py
@@ -14,3 +14,44 @@ def lookup_via_npi(request, npi):
 def lookup_via_license(request, number):
     l = get_object_or_404(License, number=number)
     return HttpResponse(l.as_json(), mimetype="application/json")
+    
+def home(request):
+    
+    response = """
+Welcome to another instance of the Medical Licsense Verification System (MLVS).
+
+This server is configured for the state of %s.
+
+This is a reference implementation of a simple RESTFul web service that returns
+license status information
+
+Query MLVS with the provider's NPI or the state issued license number. Use HTTP
+GET to query like so:
+
+     https://[SERVER]/npi/[NPI]
+     
+             -or-
+     
+     https://[SERVER]/license/[LICENSE]     
+
+If the HTTP response code is 200, then a JSON response containing the status
+information will be returned.  Below is an example.
+
+    {
+    "first_name": "Fred",
+    "last_name": "Flinstone",
+    "state": "WV",
+    "license_type": "MD",
+    "number": "2342345345",
+    "npi": "1223353456",
+    "status": "ACTIVE",
+    "created_at": "2013-12-30",
+    "updated_at": "2013-12-30"
+    }
+
+
+If the HTTP response code is 404 or otherwise not 200, there is no answer on file
+and the body of the response can be ignored because it is irrelevant.
+    """ % (settings.DEFAULT_STATE)
+
+    return HttpResponse(response, mimetype="text/plain")
